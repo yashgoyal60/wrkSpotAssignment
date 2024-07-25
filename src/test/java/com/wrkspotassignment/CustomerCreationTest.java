b3e7ebcfe8f0e44b30b4a6b85b0744a5f18a42ab
yashgoyal <yashgoyal60@gmail.com>
2024-07-26 02:32:02 +0530

    test case updated

--- src/test/java/com/wrkspotassignment/CustomerCreationTest.java
+++ src/test/java/com/wrkspotassignment/CustomerCreationTest.java
@@ -3,12 +3,26 @@ package com.wrkspotassignment;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wrkspotassignment.model.dto.Customer;
+import com.wrkspotassignment.repository.AddressRepository;
+import com.wrkspotassignment.repository.CustomerRepository;
+import com.wrkspotassignment.service.CustomerService;
+import com.wrkspotassignment.service.kafka.KafkaCustomerProducer;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ResourceLoader;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class CustomerCreationTest {
@@ -19,16 +33,27 @@ class CustomerCreationTest {
 	ResourceLoader resourceLoader;
 
 
+	@Autowired
+	CustomerService customerService ;
+
+	@MockBean
+	CustomerRepository customerRepository;
+	@MockBean
+	KafkaCustomerProducer kafkaCustomerProducer;
+
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void customerCreation_Pass() throws IOException {
+	public void customerCreation_Pass() throws IOException, ExecutionException, InterruptedException, TimeoutException {
 		String fileName = "CustomerCreationRequest.json";
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		Customer customer = objectMapper.readValue(resourceLoader.getResource("classpath:assets/" + fileName).getInputStream(), Customer.class);
+		Mockito.when(kafkaCustomerProducer.sendCustomerCreationEvent(List.of(customer))).thenReturn(Mockito.anyBoolean());
+		Mockito.when(customerRepository.saveAll(List.of(customer))).thenReturn(List.of(customer));
+		assertEquals(customerService.createCustomer(List.of(customer)).size(), 0);
 
 	}
 
